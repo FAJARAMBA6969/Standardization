2122492c578c9491a3132cf19e4b9698c02d68f2
FAJAR ARSALAN NAUFAL <fajararsalannaufal@gmail.com>
2025-07-20 15:23:25 +0700

    Update pembuatan_Lbs.py

--- page/pembuatan_Lbs.py
+++ page/pembuatan_Lbs.py
@@ -46,7 +46,7 @@ def tampil():
             [data-testid="stAppViewContainer"] {
                 background:
                     radial-gradient(circle at top left, #182438 20%, transparent 80%),
-                    radial-gradient(circle at bottom right, #182438 20%, transparent 80%),
+                    radial-gradient(circle at bottom right, #182438 3%, transparent 80%),
                     radial-gradient(circle at top right, #00948a 10%, transparent 70%),
                     linear-gradient(180deg, #6884a1, #bde3ff);
             color: #c8dee8;
