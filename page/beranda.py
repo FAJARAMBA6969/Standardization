5956861c6f638f44e7fbec1f99b0a8a2d7511a20
FAJAR ARSALAN NAUFAL <fajararsalannaufal@gmail.com>
2025-07-20 11:41:38 +0700

    initial commit

---
+++ page/beranda.py
@@ -0,0 +1,75 @@
+import streamlit as st
+import base64
+
+def get_base64_image(image_path):
+    with open(image_path, "rb") as img_file:
+        encoded = base64.b64encode(img_file.read()).decode()
+        return f"data:image/png;base64,{encoded}"
+def tampil():
+    img_data = get_base64_image("logo/logot.png")
+
+    st.markdown(
+        f"""
+        <style>
+        [data-testid="stHeader"] {{
+            background-color: rgba(0, 0, 0, 0);    
+        }}
+        @import url('https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700&family=Space+Grotesk:wght@500;700&display=swap');
+            
+        html, body, [class*="css"] {{
+            font-family: 'Poppins', sans-serif;
+        }}
+        .logo-section {{
+            display: flex;
+            flex-direction: column;
+            align-items: center;
+            margin-top: 30px;
+        }}
+        .logo-title {{
+            font-family: 'Space Grotesk', sans-serif;
+            font-size: 50px;
+            font-weight: bold;
+            text-align: center;
+            color: #c8dee8;
+            margin-bottom: 0px;
+            text-shadow: 1px 1px 3px rgba(0, 0, 0, 0.5)
+        }}
+        .logo-wrapper {{
+            display: flex;
+            justify-content: center;
+            align-items: center;
+        }}
+        [data-testid="stAppViewContainer"] {{
+            background:
+                radial-gradient(circle at top left, #182438 20%, transparent 80%),
+                radial-gradient(circle at bottom right, #182438 20%, transparent 80%),
+                radial-gradient(circle at top right, #00948a 10%, transparent 70%),
+                linear-gradient(180deg, #6884a1, #bde3ff);
+        color: #c8dee8;
+        }}
+        .logo-wrapper img {{
+            width: 300px;
+            height: 300px;
+            border-radius: 50%;
+            object-fit: cover;
+        }}
+        </style>
+        """,
+        unsafe_allow_html=True
+    )
+
+    st.markdown(f"""
+    <div class="logo-selection">
+        <div class="logo-title">Calculator of Standardization</div>
+        <div class="logo-wrapper">
+            <img src="{img_data}" alt="Logo Bulat">
+        </div>
+    </div>           
+    """, unsafe_allow_html=True)
+
+    st.markdown("""
+    <div style="height: 3px; background-color: rgba(255, 255, 255, 0.1); margin-top: 0px; margin-bottom: 20px;"></div>
+    """, unsafe_allow_html=True)
+    st.write(
+        "Calculator of Standaritation adalah web online gratis yang dirancang untuk memudahkan pengguna dalam menghitung konsentrasi larutan standar titrimetri.")
+    st.write("silahkan pilih metode perhitungan yang sesuai, kemudian ikuti petunjuk yang ditampilkan dilayar!")
---
+++ st_app.py
@@ -0,0 +1,41 @@
+import streamlit as st
+from streamlit_option_menu import option_menu
+from page import beranda, Pembuatan_Ls, pembuatan_Lbs, Menghitung_s
+
+
+st.set_page_config(page_title="Calculator of Standardization", page_icon="🧪", layout="wide")
+
+st.markdown("""
+<style>
+    section[data-testid="stSidebar"] {
+        background-color: rgba(0, 0, 0, 0.5);
+        box-shadow: 0 0 20px rgba(0,0,0,0.3), 0 0 20px rgba(0,0,0,0.3);
+    }
+</style>
+""", unsafe_allow_html=True)
+
+with st.sidebar:
+    selected = option_menu(
+        menu_title="Menu",
+        options=[
+            "Beranda",
+            "Pembuatan Larutan Standar",
+            "Pengenceran Larutan Standar",
+            "Perhitungan Standarisasi"
+        ],
+        icons=["house", "eyedropper", "droplet", "calculator"],
+        default_index=0
+    )
+
+st.sidebar.markdown("""
+    <div style="height: 3px; background-color: rgba(255, 255, 255, 0.15); margin-top: 0px; margin-bottom: 20px;"></div>
+    """, unsafe_allow_html=True)
+
+if selected == "Beranda":
+    beranda.tampil()
+elif selected == "Pembuatan Larutan Standar":
+    pembuatan_Lbs.tampil()
+elif selected == "Pengenceran Larutan Standar":
+    Pembuatan_Ls.tampil()
+elif selected == "Perhitungan Standarisasi":
+    Menghitung_s.tampil()
